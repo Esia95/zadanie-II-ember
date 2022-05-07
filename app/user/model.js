af23636217a214f73bc3d33ced74783707f44342
Estera Nowak <esteranowak95@gmail.com>
2022-05-07 13:43:37 +0200

    update-post-owner

--- app/user/model.js
+++ app/user/model.js
@@ -1,15 +1,10 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class UserModel extends Model {
   @attr('string') username;
   @attr('string') password;
   @attr('string') email;
-  @attr('boolean', {
-    defaultValue: false,
-  })
-  isDeleted;
-  @attr('boolean', {
-    defaultValue: false,
-  })
-  isAdmin;
+  @attr('boolean', { defaultValue: false }) isDeleted;
+  @attr('boolean', { defaultValue: false }) isAdmin;
+  @hasMany('post') posts;
 }
