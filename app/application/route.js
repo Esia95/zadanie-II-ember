fd4632174c793fd6c82a67d720f7a17819993b44
Estera Nowak <esteranowak95@gmail.com>
2022-05-07 14:31:47 +0200

    adding-photo

--- app/application/route.js
+++ app/application/route.js
@@ -5,17 +5,21 @@ export default class ApplicationRoute extends Route {
   @service store;
 
   async beforeModel() {
-    window.localStorage.clear();
     const user1 = {
+      id: 1,
       username: 'admin',
       password: 'admin123',
       email: 'admin@admin.com',
       isAdmin: true,
+      photoURL: 'https://memegenerator.net/img/images/71831023/mapa-janusz.jpg',
     };
     const user2 = {
+      id: 2,
       username: 'user',
       password: 'user',
       email: 'user@user.com',
+      photoURL:
+        'https://data3.cupsell.pl/upload/generator/266414/640x420/5015943_print_1.png?resize=max_sizes&key=55f9a22768eed085006592c1174c0235',
     };
 
     const user1Model = this.store.createRecord('user', user1);
@@ -25,16 +29,19 @@ export default class ApplicationRoute extends Route {
     await user2Model.save();
 
     const post1 = {
+      id: 1,
       title: 'Test1',
       body: 'Zawartość tekstowa',
       owner: user1Model,
     };
     const post2 = {
+      id: 2,
       title: 'Test2',
       body: 'Zawartość tekstowa2',
       owner: user1Model,
     };
     const post3 = {
+      id: 3,
       title: 'Test3',
       body: 'Zawartość tekstowa3',
       owner: user2Model,
