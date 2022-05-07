5328fee228405586ddca12350f9749827f615dd1
Estera Nowak <esteranowak95@gmail.com>
2022-05-07 12:48:43 +0200

    adding-model-post-and-updading-show-post

--- app/application/route.js
+++ app/application/route.js
@@ -18,9 +18,29 @@ export default class ApplicationRoute extends Route {
       email: 'user@user.com',
     };
 
+    const post1 = {
+      title: 'Test1',
+      body: 'Zawartość tekstowa',
+    };
+    const post2 = {
+      title: 'Test2',
+      body: 'Zawartość tekstowa2',
+    };
+    const post3 = {
+      title: 'Test3',
+      body: 'Zawartość tekstowa3',
+    };
+
     const user1Model = this.store.createRecord('user', user1);
     const user2Model = this.store.createRecord('user', user2);
+    const post1Model = this.store.createRecord('post', post1);
+    const post2Model = this.store.createRecord('post', post2);
+    const post3Model = this.store.createRecord('post', post3);
+
     await user1Model.save();
     await user2Model.save();
+    await post1Model.save();
+    await post2Model.save();
+    await post3Model.save();
   }
 }
