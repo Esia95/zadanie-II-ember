2189c0ecbe971e11292e12afcb5e7aca790659dc
Estera Nowak <esteranowak95@gmail.com>
2022-05-14 14:25:05 +0200

    adding-new-users

--- app/register/route.js
+++ app/register/route.js
@@ -1,3 +1,22 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { storageFor } from 'ember-local-storage';
+//import { action } from '@ember/object';
 
-export default class RegisterRoute extends Route {}
+export default class RegisterRoute extends Route {
+  @service store;
+  @service router;
+  @storageFor('logged-as') loggedAs;
+
+  beforeModel() {
+    const userId = this.loggedAs.get('id');
+    if (userId) {
+      this.router.tarnsitionTo('home');
+      return;
+    }
+  }
+
+  model() {
+    return this.store.createRecord('user');
+  }
+}
