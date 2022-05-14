164c4c9e10fa5168fe95dacab4cf003873066386
Estera Nowak <esteranowak95@gmail.com>
2022-05-14 12:24:31 +0200

    adding-login

--- app/home/route.js
+++ app/home/route.js
@@ -2,14 +2,14 @@ import Route from '@ember/routing/route';
 import { storageFor } from 'ember-local-storage';
 import { inject as service } from '@ember/service';
 
-export default class LoginRoute extends Route {
+export default class HomeRoute extends Route {
   @service router;
   @storageFor('logged-as') loggedAs;
 
   beforeModel() {
     const userId = this.loggedAs.get('id');
-    if (userId) {
-      this.router.tarnsitionTo('home');
+    if (!userId) {
+      this.router.tarnsitionTo('/login');
       return;
     }
   }
