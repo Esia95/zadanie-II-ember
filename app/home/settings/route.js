f15f157d608b321992b0d0cb840109fd463580d1
Estera Nowak <esteranowak95@gmail.com>
2022-05-28 13:20:39 +0200

    updated settings

--- app/home/settings/route.js
+++ app/home/settings/route.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+export default class SettingsRoute extends Route {
+  @service session;
 
-export default class SettingsRoute extends Route {}
+  model() {
+    return this.session.currentUser;
+  }
+}
