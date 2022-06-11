67ace8ff88f5eef26e3318edc047cadb29e4d2c4
Estera Nowak <esteranowak95@gmail.com>
2022-06-11 14:47:34 +0200

    fix: filtered by date

--- app/post/model.js
+++ app/post/model.js
@@ -1,4 +1,5 @@
 import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
+import moment from 'moment';
 
 export default class PostModel extends Model {
   @attr('string') title;
@@ -6,5 +7,5 @@ export default class PostModel extends Model {
   @attr('boolean', { defaultValue: false }) isDeleted;
   @belongsTo('user', { autoSave: true }) owner;
   @hasMany('like') likes;
-  @attr('date', { defaultValue: () => new Date() }) createdAt;
+  @attr('date', { defaultValue: () => moment() }) createdAt;
 }
