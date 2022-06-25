89adb6cd204938c93855cc9e9875cc9ad4cd3242
Estera Nowak <esteranowak95@gmail.com>
2022-06-25 14:30:35 +0200

    Creating shared user selector component

--- app/home/posts/controller.js
+++ app/home/posts/controller.js
@@ -2,20 +2,12 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import moment from 'moment';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 
 export default class HomePostsController extends Controller {
-  @service store;
-
   @tracked dateFrom;
   @tracked dateTo;
   @tracked sort;
   @tracked selectedAuthors = [];
-
-  constructor() {
-    super(...arguments);
-    this.authors = this.store.findAll('user');
-  }
 
   queryParams = ['dateFrom', 'dateTo', 'sort'];
 
@@ -140,6 +132,6 @@ export default class HomePostsController extends Controller {
 
   @action
   chooseAuthors(author) {
-    this.selectedAuthors = author;
+    this.selectedAuthors = Array.isArray(author) ? author : [author];
   }
 }
