9cd3b563a9869ad3fa76123ddaa74b55be015b1d
Estera Nowak <esteranowak95@gmail.com>
2022-06-25 13:26:07 +0200

    Adding power select

--- app/home/posts/controller.js
+++ app/home/posts/controller.js
@@ -2,11 +2,20 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import moment from 'moment';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class HomePostsController extends Controller {
+  @service store;
+
   @tracked dateFrom;
   @tracked dateTo;
   @tracked sort;
+  @tracked selectedAuthors = [];
+
+  constructor() {
+    super(...arguments);
+    this.authors = this.store.findAll('user');
+  }
 
   queryParams = ['dateFrom', 'dateTo', 'sort'];
 
@@ -116,5 +125,10 @@ export default class HomePostsController extends Controller {
     }
 
     this.sort = undefined;
+  }
+
+  @action
+  chooseAuthors(author) {
+    this.selectedAuthors = author;
   }
 }
