acf51f954b75e358325a78aee27c2d7000eb430b
Estera Nowak <esteranowak95@gmail.com>
2022-07-02 15:49:26 +0200

    adding pacakage faker and test component

---
+++ tests/integration/components/user/details/component-test.js
@@ -0,0 +1,28 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { faker } from '@faker-js/faker';
+
+module('Integration | Component | user/details', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    faker.setLocale('pl');
+    const user = {
+      id: '1',
+      username: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      photoURL: faker.image.avatar(),
+    };
+    this.set('user', user);
+    await render(hbs`<User::Details @user={{this.user}} />`);
+    await this.pauseTest();
+
+    assert.dom('[data-test-username]').hasText(user.username);
+    assert.dom('[data-test-id]').hasText(user.id);
+    assert.dom('[data-test-email]').hasText(user.email);
+    assert.dom('[data-test-password]').hasText(user.password);
+  });
+});
