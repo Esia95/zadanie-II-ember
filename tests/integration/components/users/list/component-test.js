c508cd76467a8ed9e76231d01c53101cbaed5e50
Estera Nowak <esteranowak95@gmail.com>
2022-07-09 13:22:29 +0200

    create test to component user list

---
+++ tests/integration/components/users/list/component-test.js
@@ -0,0 +1,23 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Integration | Component | users/list', function (hooks) {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  test('it renders', async function (assert) {
+    const users = this.server.createList('user', 20);
+    this.set('users', users);
+    await render(hbs`<Users::List @users={{this.users}} />`);
+
+    users.map((user, index) => {
+      assert.dom(`[data-test-id="${index}"]`).hasText(user.id);
+      assert.dom(`[data-test-username="${index}"]`).hasText(user.username);
+      assert.dom(`[data-test-email="${index}"]`).hasText(user.email);
+      assert.dom(`[data-test-password="${index}"]`).hasText(user.password);
+    });
+  });
+});
